1cacb5b92ba37053641b371ad366ab56663fab64
Choi-Yuchan <cor330@gmail.com>
2023-07-11 18:02:49 +0900

    test for practice java

--- src/Test.java
+++ src/Test.java
@@ -1,46 +1,18 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.InputStreamReader;
-import java.io.Writer;
-import java.net.URL;
-import java.net.URLConnection;
 import java.util.HashSet;
-import java.util.Scanner;
 import java.util.Set;
-
 
 public class Test {
 	public static void main(String[] args) {
-		new UrlConnect();
-    }
-
-}
-
-class UrlConnect{
-	public UrlConnect() {
-		String code = null;
-		System.out.println("웹 주소를 입력하세요.");
-		Scanner scanner = new Scanner(System.in);
-		String address = scanner.next();
-		
-		try {
-			URL url = new URL(address);
-			URLConnection connection = url.openConnection();
-			BufferedReader webData = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-			FileWriter writer = new FileWriter("website.html", false);
-			
-			while((code = webData.readLine())!=null) {
-				System.out.println(code);
-				writer.write(code);
+		for (int i = 0; i <= 11; i++) {
+			if(i%11 == 0) {
+				for(int j = 1; j<=7; j++) {
+					if(j%7 == 0) {
+						System.out.println(i*j);
+						break;
+					}
+				}
 			}
-			System.out.println("The End");
-			
-			writer.close();
-			webData.close();
-		} catch (Exception e) {
-			// TODO: handle exception
 		}
 	}
+
 }
---
+++ src/edu/global/prj/ChatGPT.java
@@ -0,0 +1,10 @@
+package edu.global.prj;
+
+public class ChatGPT {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
